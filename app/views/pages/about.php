7d055251926cc69bce0728ede73bf5088c0cb59e
slfassurd <rose.michael71@yahoo.com>
2020-05-18 14:44:02 -0600

    initial commit

---
+++ app/views/pages/about.php
@@ -0,0 +1,34 @@
+<?php require APPROOT . '/views/inc/header.php'; ?>
+
+
+<section id="showcase">
+			<div class="overlay">
+				<div class="container">
+							<div class="d-flex flex-column align-items-center justify-content-center" style="height:100%">
+								<h1>About Me</h1>
+								<h2>by Michael Rose</h2>
+								<h3>This is a site that is in progress....</h3>
+								<div class="text-left">
+									<p>In another life, I was working at Packard Bell/NEC customer service. I had gotten tired of the corporate life, and wanted a change.
+										That is when I decided to change my career.</p>
+									<p>I'm a truck driver by trade.  In 2019 I had decided that it was time for me to own my own business.  For 10 months I was 
+									purchasing my own truck and leasing back to KMB Brothers.  The engine blew and I couldn't afford to fix it.</p>
+									<p>After that catastrophe, I had become unemployed for a couple of months.  I had attempted to get a loan to either
+										fix my truck or get another one.  However, I didn't have any savings and didn't have a down payment.</p>
+									<p>While I was attempting to save my business, I had continued looking for work.  I was doing some temporary work
+										and even washed some dishes.  Eventually, I had found Sunrise Express, a trucking company out of Nebraska.  They would
+										allow me to keep my newly aquired puppy dog Nuvo.</p>
+									<p>Even though I was up and running again, I wanted to save my business.  I was doing research on Web Development and decided
+										that I would give it a try.</p>
+									<p>And that is my story.  Right now I'm working on this site and still learning.  I can bring a whole new perspective to the 
+										industry.  I have worked various manual labor jobs in the past.  With the experience that I have with computers, I'll bring 
+										a new perspective to the business.</p>
+								</div>
+								<h2>The story continues...</h2>
+							</div>
+				</div>
+			</div>	
+		</section>
+
+
+<?php require APPROOT . '/views/inc/footer.php'; ?>
---
+++ app/views/pages/index.php
@@ -0,0 +1,15 @@
+<?php require APPROOT . '/views/inc/header.php'; ?>
+
+<section id="showcase">
+            <div class='overlay'>   
+                <div class="container">
+                    <div class="d-flex flex-column align-items-center justify-content-center" style='height:100vh'>
+                        <img src="<?php echo(URLROOT); ?>/img/logo_leaf.png" alt="" title="" height="100px">
+                        <h2 class="display-3">Welcome to my website</h2>
+                        <a>I'm ALMOST ready to open up!!!!</a>
+                    </div>
+                </div>
+            </div>             
+</section>
+
+<?php require APPROOT . '/views/inc/footer.php'; ?>
